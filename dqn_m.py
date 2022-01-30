861996f3dff2fa6f54a786bb00c28ad0c98490fc
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-01-30 12:41:04 +0800

    1. add parser 2. add config_json 3. fix scope names 4. save checkpoints 5. fix tf.Summary.Value and fix 'episode_info' 6. adjust structrue of agent slightly

--- dqn_m.py
+++ dqn_m.py
@@ -1,12 +1,12 @@
 import gym
-import itertools
 import numpy as np
 import os
 import random
-import sys
 import tensorflow as tf
 import time
+import json
 
+from argparse import ArgumentParser
 from lib.dqn_utils_m import *
 
 def linearly_decaying_epsilon(decay_period, step, warmup_steps, epsilon):
@@ -47,15 +47,15 @@ class NatureDQNNetwork(tf.keras.Model):
 		# Setting names of the layers manually to make variable names more similar
 		# with tf.slim variable names/checkpoints.
 		self.conv1 = tf.keras.layers.Conv2D(32, [8, 8], strides=4, padding='same',
-			  data_format='channels_first', activation=activation_fn, name='Conv')
+			  data_format='channels_first', activation=activation_fn, name='conv1')
 		self.conv2 = tf.keras.layers.Conv2D(64, [4, 4], strides=2, padding='same',
-			  data_format='channels_first', activation=activation_fn, name='Conv')
+			  data_format='channels_first', activation=activation_fn, name='conv2')
 		self.conv3 = tf.keras.layers.Conv2D(64, [3, 3], strides=1, padding='same',
-			  data_format='channels_first', activation=activation_fn, name='Conv')
+			  data_format='channels_first', activation=activation_fn, name='conv3')
 		self.flatten = tf.keras.layers.Flatten()
 		self.dense1 = tf.keras.layers.Dense(512, activation=activation_fn,
-											name='fully_connected')
-		self.dense2 = tf.keras.layers.Dense(self.num_actions, name='fully_connected')
+											name='fc1')
+		self.dense2 = tf.keras.layers.Dense(self.num_actions, name='fc2')
 
 	def call(self, state):
 		"""Creates the output tensor/op given the state tensor as input.
@@ -83,7 +83,7 @@ class Agent():
 	def __init__(self,
 				 sess,
 				 num_actions,
-				 experiment_dir=None,
+				 exp_dir=None,
 				 replay_capacity=1000000,
 				 replay_min_size=50000,
 				 update_period=4,
@@ -92,68 +92,95 @@ class Agent():
 				 epsilon_decay_period=1000000,
 				 gamma=0.99,
 				 batch_size=32,
+				 max_tf_checkpoints_to_keep=4,
+				 # https://www.tensorflow.org/api_docs/python/tf/compat/v1/train/RMSPropOptimizer
+				 # #migrate-to-tf2_2
+				 optimizer=tf.train.RMSPropOptimizer(learning_rate=0.00025, \
+													 decay=0.95, \
+													 epsilon=1e-6, \
+													 centered=False),
                  summary_writer=None,
                  summary_writing_frequency=500):
-		# log here
+		self.config = {}
+		self.config['num_actions'] = num_actions
+		self.config['replay_capacity'] = replay_capacity
+		self.config['replay_min_size'] = replay_min_size
+		self.config['update_period'] = update_period
+		self.config['target_update_period'] = target_update_period
+		self.config['epsilon_end'] = epsilon_end
+		self.config['gamma'] = gamma
+		self.config['batch_size'] = batch_size
+		self.config['max_tf_checkpoints_to_keep'] = max_tf_checkpoints_to_keep
+		self.config['optimizer'] = optimizer.get_name()
+		self.config['summary_writing_frequency'] = summary_writing_frequency
 		self.sess = sess
 		self.num_actions = num_actions
+		self.exp_dir = exp_dir
 		self.replay_min_size = replay_min_size
 		self.update_period = update_period
 		self.target_update_period = target_update_period
 		self.epsilon_end = epsilon_end
 		self.epsilon_decay_period = epsilon_decay_period
 		self.gamma = gamma
-		self.training_steps = 0
+		self.max_tf_checkpoints_to_keep = max_tf_checkpoints_to_keep
+		self.optimizer = optimizer
 		self.summary_writer = summary_writer
 		self.summary_writing_frequency = summary_writing_frequency
+		self.training_steps = 0
 		self.replay_memory = ReplayMemory(memory_size=replay_capacity, batch_size=batch_size)
 		self.history = History()
-		self.optimizer = tf.train.RMSPropOptimizer(learning_rate=0.00025, decay=0.95, \
-														epsilon=1e-6, centered=False)
 		self._build_networks()
 		self._train_op = self._build_train_op()
 		self._sync_qt_ops = self._build_sync_op()
 		self._state_processer = self._build_state_processer()
+		self._saver = self._build_saver()
 
 		if self.summary_writer is not None:
 			# All tf.summaries should have been defined prior to running this.
 			self._merged_summaries = tf.summary.merge_all()
 
 	def _build_networks(self):
-		self.state_ph = tf.placeholder(shape=[None, 4, 84, 84], dtype=tf.uint8, name="state_ph")
-		self.next_state_ph = tf.placeholder(shape=[None, 4, 84, 84], dtype=tf.uint8, name="next_state_ph")
-		self.online_network = NatureDQNNetwork(num_actions=self.num_actions, name="Online")
-		self.target_network = NatureDQNNetwork(num_actions=self.num_actions, name="Target")
-		self.online_net_output = self.online_network(self.state_ph)
-		self.target_net_output = self.target_network(self.next_state_ph)
-		self.q_argmax = tf.argmax(self.online_net_output, axis=1)[0]
-		self.next_q_max = tf.reduce_max(self.target_net_output, axis=1) # (32, 1)
-		if self.summary_writer is not None:
-			with tf.variable_scope('Q_estimate'):
-				tf.summary.scalar(name='max_q_value', tensor=tf.reduce_max(self.online_net_output))
-				tf.summary.scalar(name='avg_q_value', tensor=tf.reduce_mean(self.online_net_output))
+		self.online_network = NatureDQNNetwork(num_actions=self.num_actions, name="online")
+		self.target_network = NatureDQNNetwork(num_actions=self.num_actions, name="target")
+	
+	# Note: Required to be called after _build_train_op(), otherwise return []
+	def _get_var_list(self, name='online'):
+		scope = tf.get_default_graph().get_name_scope()
+		trainable_variables = tf.get_collection(
+			tf.GraphKeys.TRAINABLE_VARIABLES,
+			scope=os.path.join(scope, name))
+		return trainable_variables
 
 	def _build_train_op(self):
+		self.state_ph = tf.placeholder(shape=[None, 4, 84, 84], dtype=tf.uint8, name="state_ph")
+		self.next_state_ph = tf.placeholder(shape=[None, 4, 84, 84], dtype=tf.uint8, name="next_state_ph")
 		self.actions_ph = tf.placeholder(shape=[None,], dtype=tf.int32, name="actions_ph")
 		self.rewards_ph = tf.placeholder(shape=[None,], dtype=tf.float32, name="rewards_ph")
 		self.terminals_ph = tf.placeholder(shape=[None,], dtype=tf.float32, name="terminals_ph")
 
-		next_q_max = tf.squeeze(self.next_q_max) # (32, )
-		target = self.rewards_ph + self.gamma * (1 - self.terminals_ph) * next_q_max # (32, )
+		self.online_net_output = self.online_network(self.state_ph) # (32, 4)
+		self.target_net_output = self.target_network(self.next_state_ph) # (32, 4)
+		self.q_argmax = tf.argmax(self.online_net_output, axis=1)[0] # (1, ) => ()
+
+		next_q_max = tf.reduce_max(self.target_net_output, axis=1) # (32, 4) => (32,)
+		target = self.rewards_ph + self.gamma * (1 - self.terminals_ph) * next_q_max # (32,)
 		target_nograd = tf.stop_gradient(target)
 
-		q_value_chosen = tf.squeeze(
-						     tf.gather(self.online_net_output,
-							 	tf.expand_dims(self.actions_ph, axis=-1), axis=1, batch_dims=1))
+		q_value_chosen = tf.squeeze( \
+						     tf.gather(self.online_net_output, \
+							 	tf.expand_dims(self.actions_ph, axis=-1), axis=1, batch_dims=1)) # (32,)
 
 		losses = tf.losses.huber_loss(
 			target_nograd, q_value_chosen, reduction=tf.losses.Reduction.NONE)
 		loss = tf.reduce_mean(losses)
 		if self.summary_writer is not None:
-			with tf.variable_scope('Losses'):
+			with tf.variable_scope('losses'):
 				tf.summary.scalar(name='huberloss', tensor=loss)
-		return self.optimizer.minimize(loss)
-	
+			with tf.variable_scope('q_estimate'):
+				tf.summary.scalar(name='max_q_value', tensor=tf.reduce_max(self.online_net_output))
+				tf.summary.scalar(name='avg_q_value', tensor=tf.reduce_mean(self.online_net_output))
+		return self.optimizer.minimize(loss, var_list=self._get_var_list())
+
 	def _build_state_processer(self):
 		self.raw_state = tf.placeholder(shape=[210, 160, 3], dtype=tf.uint8)
 		self.output = tf.image.rgb_to_grayscale(self.raw_state)
@@ -169,17 +196,19 @@ class Agent():
 		"""
 		# Get trainable variables from online and target DQNs
 		sync_qt_ops = []
-		scope = tf.get_default_graph().get_name_scope()
-		trainables_online = tf.get_collection(
-			tf.GraphKeys.TRAINABLE_VARIABLES,
-			scope=os.path.join(scope, 'Online'))
-		trainables_target = tf.get_collection(
-			tf.GraphKeys.TRAINABLE_VARIABLES,
-			scope=os.path.join(scope, 'Target'))
+		trainable_online = self._get_var_list()
+		trainable_target = self._get_var_list('target')
 
-		for (w_online, w_target) in zip(trainables_online, trainables_target):
+		for (w_online, w_target) in zip(trainable_online, trainable_target):
 			sync_qt_ops.append(w_target.assign(w_online, use_locking=True))
 		return sync_qt_ops
+	
+	def _build_saver(self):
+		self.checkpoint_dir = os.path.join(self.exp_dir, 'checkpoints')
+		if not os.path.exists(self.checkpoint_dir):
+			os.makedirs(self.checkpoint_dir)
+		return tf.train.Saver(var_list=self._get_var_list(), \
+				max_to_keep=self.max_tf_checkpoints_to_keep)
 
 	def select_action(self):
 		epsilon = linearly_decaying_epsilon(
@@ -217,6 +246,12 @@ class Agent():
 			if self.training_steps % self.target_update_period == 0:
 				self.sess.run(self._sync_qt_ops)
 
+			if (self.training_steps % 1000000 == 999999):
+				self._saver.save(
+					self.sess,
+					os.path.join(self.checkpoint_dir, 'tf_ckpt'),
+					global_step=self.training_steps)
+
 		self.training_steps += 1
 
 	def reset_history(self, observation):
@@ -225,21 +260,37 @@ class Agent():
 			self.history.add(state)
 
 
-def main(num_episodes=10000):
+def main(args):
+	name = args.name
+	tag = args.tag
+	num_episodes = args.num_ep
+	# Create environment
 	env = gym.make('Breakout-v0')
 	num_actions = env.action_space.n
-	results_dir = os.path.abspath("/data/hanjl/my_results/{}".format(env.spec.id + "-" + \
-										time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))))
-	summary_dir = os.path.join(results_dir, "tf1_summaries")
+	# results dir setting
+	timestamp = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+	exp_name = name or (tag + '-' + timestamp)
+	results_dir = os.path.join(f"/data/hanjl/my_results/{env.spec.id}", exp_name)
+	summary_dir = os.path.join(results_dir, "tf1_summary")
 	if not os.path.exists(summary_dir):
 		os.makedirs(summary_dir)
 	summary_writer = tf.summary.FileWriter(summary_dir)
+	# For episode information
 	episode_lengths=np.zeros(num_episodes)
 	episode_rewards=np.zeros(num_episodes)
-	with tf.Session() as sess:
+	# Config used for recording all parameters
+	config = {}
+	config['exp_name'] = exp_name
+	config['time'] = timestamp
+	config['env'] = env.spec.id
+	config['results_dir'] = results_dir
+	# Tf.Session()
+	gpu_options = tf.GPUOptions(allow_growth=True)	
+	with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+		# Instantiate Agent and initialize all variables
 		agent = Agent(sess,
+					  exp_dir=results_dir,
 					  num_actions=num_actions,
-					  experiment_dir=None,
 					  replay_capacity=1000000,
 					  replay_min_size=50000,
 					  update_period=4,
@@ -248,12 +299,25 @@ def main(num_episodes=10000):
 					  epsilon_decay_period=1000000,
 					  gamma=0.99,
 					  batch_size=32,
+					  # https://www.tensorflow.org/api_docs/python/tf/compat/v1/train/RMSPropOptimizer
+					  # #migrate-to-tf2_2
+					  optimizer=tf.train.RMSPropOptimizer(learning_rate=0.00025, \
+						  								  decay=0.95, \
+														  epsilon=1e-6, \
+														  centered=False),
 					  summary_writer=summary_writer,
                       summary_writing_frequency=500)
 		sess.run(tf.global_variables_initializer())
+		# Save config.json
+		config['agent_config'] = agent.config
+		config_json = json.dumps(config, sort_keys=False, indent=4, separators=(',', ': '))
+		with open(os.path.join(results_dir, "config.json"), 'w') as out:
+			out.write(config_json)
+		# Start iteration of episodes
 		for i in range(num_episodes):
 			observation = env.reset()
 			agent.reset_history(observation)
+			# One episode
 			while True:
 				print(f"\r@episode: {i}/{num_episodes}, length: {episode_lengths[i]}", end='')
 				action = agent.select_action()
@@ -263,12 +327,19 @@ def main(num_episodes=10000):
 				episode_lengths[i] += 1
 				if done:
 					break
-			episode_summary = tf.Summary()
-			episode_summary.value.add(simple_value=episode_rewards[i], node_name="episode_reward", tag="reward")
-			episode_summary.value.add(simple_value=episode_lengths[i], node_name="episode_length", tag="length")
+			# Summary episode infomation
+			episode_summary = tf.Summary(value=[
+				tf.Summary.Value(simple_value=episode_rewards[i], tag="episode_info/reward"),
+				tf.Summary.Value(simple_value=episode_lengths[i], tag="episode_info/length")
+			])
 			summary_writer.add_summary(episode_summary, i)
-			summary_writer.flush()
+			# summary_writer.flush()
 			print(f"\nreward: {episode_rewards[i]}")
 
 if __name__ == '__main__':
-	main()
+	parser = ArgumentParser()
+	parser.add_argument('--tag', type=str, default='test', help='Used as exp_name')
+	parser.add_argument('--name', type=str, default=None, help='Used as exp_name')
+	parser.add_argument('--num_ep', type=int, default=10000, help='Number of episodes')
+	args, unknown_args = parser.parse_known_args()
+	main(args)
