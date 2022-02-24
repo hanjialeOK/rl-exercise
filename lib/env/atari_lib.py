4b49ff46d3a44859b9c1fb633f7b02a9012ba9b6
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-02-24 02:51:11 +0000

    Refactor code. It's a litte faster than atari_wrappers.

--- lib/env/atari_lib.py
+++ lib/env/atari_lib.py
@@ -2,23 +2,12 @@ import numpy as np
 import random
 import cv2
 import gym
-from gym.spaces.box import Box
 
 from lib.utils.json_tools import json_serializable
 
 
-def create_atari_environment(game_name=None, sticky_actions=True):
-    """Wraps an Atari 2600 Gym environment with some basic preprocessing.
-    This preprocessing matches the guidelines proposed in Machado et al. (2017),
-    "Revisiting the Arcade Learning Environment: Evaluation Protocols and Open
-    Problems for General Agents".
-    The created environment is the Gym wrapper around the Arcade Learning
-    Environment.
-    The main choice available to the user is whether to use sticky actions or not.
-    Sticky actions, as prescribed by Machado et al., cause actions to persist
-    with some probability (0.25) when a new command is sent to the ALE. This
-    can be viewed as introducing a mild form of stochasticity in the environment.
-    We use them by default.
+def create_atari_environment(game_name=None):
+    """
     Args:
         game_name: str, the name of the Atari 2600 domain.
         sticky_actions: bool, whether to use sticky_actions as per Machado et al.
@@ -37,120 +26,83 @@ def create_atari_environment(game_name=None, sticky_actions=True):
 
 class AtariPreprocessing(object):
     """A class implementing image preprocessing for Atari 2600 agents.
-    Specifically, this provides the following subset from the JAIR paper
-    (Bellemare et al., 2013) and Nature DQN paper (Mnih et al., 2015):
         * Frame skipping (defaults to 4).
         * Terminal signal when a life is lost (off by default).
         * Grayscale and max-pooling of the last two frames.
         * Downsample the screen to a square image (defaults to 84x84).
-    More generally, this class follows the preprocessing guidelines set down in
-    Machado et al. (2018), "Revisiting the Arcade Learning Environment:
-    Evaluation Protocols and Open Problems for General Agents".
     """
 
-    def __init__(self, environment, frame_skip=4, terminal_on_life_loss=False,
-                 screen_size=84, max_random_start=30):
-        """Constructor for an Atari 2600 preprocessor.
-        Args:
-            environment: Gym environment whose observations are preprocessed.
-            frame_skip: int, the frequency at which the agent experiences the game.
-            terminal_on_life_loss: bool, If True, the step() method returns
-                is_terminal=True whenever a life is lost. See Mnih et al. 2015.
-            screen_size: int, size of a resized Atari 2600 frame.
-        Raises:
-            ValueError: if frame_skip or screen_size are not strictly positive.
-        """
+    def __init__(self, env, frame_skip=4, punish_on_loss=True, noop_max=30):
         self.config = json_serializable(locals())
-        if frame_skip <= 0:
-            raise ValueError('Frame skip should be strictly positive, got {}'.
-                             format(frame_skip))
-        if screen_size <= 0:
-            raise ValueError('Target screen size should be strictly positive, got {}'.
-                             format(screen_size))
 
-        self.environment = environment
-        self.terminal_on_life_loss = terminal_on_life_loss
-        self.frame_skip = frame_skip
-        self.screen_size = screen_size
-        self.max_random_start = max_random_start
+        assert env.get_action_meanings()[0] == 'NOOP'
+        assert env.get_action_meanings()[1] == 'FIRE'
+        assert len(env.get_action_meanings()) >= 3
+        self._noop_action = 0
+        self._env = env
+        self._punish_on_loss = punish_on_loss
+        self._frame_skip = frame_skip
+        self._noop_max = noop_max
+        self._width = 84
+        self._height = 84
 
-        obs_dims = self.environment.observation_space
+        obs_dims = self._env.observation_space
         # Stores temporary observations used for pooling over two successive
         # frames.
-        self.screen_buffer = [
-            np.empty((obs_dims.shape[0], obs_dims.shape[1]), dtype=np.uint8),
-            np.empty((obs_dims.shape[0], obs_dims.shape[1]), dtype=np.uint8)
-        ]
+        obs_dims = env.observation_space.shape
+        self._obs_buffer = np.zeros(
+            (2,) + (obs_dims[0], obs_dims[1]), dtype=np.uint8)
 
-        self.game_over = False
-        self.lives = 0  # Will need to be set by reset().
+        self._lives = 0
+        # Unused but necessary.
+        self.was_life_loss = False
 
     @property
     def observation_space(self):
-        # Return the observation space adjusted to match the shape of the processed
-        # observations.
-        return Box(low=0, high=255, shape=(self.screen_size, self.screen_size, 1),
-                   dtype=np.uint8)
+        return self._env.observation_space
 
     @property
     def spec(self):
-        return self.environment.spec
+        return self._env.spec
 
     @property
     def action_space(self):
-        return self.environment.action_space
+        return self._env.action_space
 
     @property
     def reward_range(self):
-        return self.environment.reward_range
+        return self._env.reward_range
 
     @property
     def metadata(self):
-        return self.environment.metadata
+        return self._env.metadata
 
     def close(self):
-        return self.environment.close()
-
-    def reset(self):
-        """Resets the environment.
-        Returns:
-            observation: numpy array, the initial observation emitted by the
-            environment.
-        """
-        self.environment.reset()
-        self.lives = self.environment.ale.lives()
-        self._fetch_grayscale_observation(self.screen_buffer[0])
-        self.screen_buffer[1].fill(0)
-        return self._pool_and_resize()
-
-    def new_random_game(self):
-        """Resets the environment.
-        Returns:
-            observation: numpy array, the initial observation emitted by the
-            environment.
-        """
-        self.environment.reset()
-        for _ in range(random.randint(0, self.max_random_start)):
-            action = random.randint(0, self.environment.action_space.n - 1)
-            self.environment.step(action)
-            self._fetch_grayscale_observation(self.screen_buffer[0])
-        self.lives = self.environment.ale.lives()
-        self.screen_buffer[1].fill(0)
-        return self._pool_and_resize()
+        return self._env.close()
 
     def render(self, mode):
-        """Renders the current screen, before preprocessing.
-        This calls the Gym API's render() method.
-        Args:
-            mode: Mode argument for the environment's render() method.
-                Valid values (str) are:
-                'rgb_array': returns the raw ALE image.
-                'human': renders to display via the Gym renderer.
-        Returns:
-            if mode='rgb_array': numpy array, the most recent screen.
-            if mode='human': bool, whether the rendering was successful.
-        """
-        return self.environment.render(mode)
+        return self._env.render(mode)
+
+    def reset(self):
+        """ Do no-op action for a number of steps in [1, noop_max]."""
+        self._env.reset()
+        # Reset recent raw observations.
+        self._obs_buffer.fill(0)
+        # Noop actions
+        noops = random.randint(1, self._noop_max)
+        obs = None
+        for i in range(noops):
+            _, _, done, _ = self._env.step(self._noop_action)
+            # Record the last two frames.
+            if i == noops - 2:
+                self._env.ale.getScreenGrayscale(self._obs_buffer[0])
+            if i == noops - 1:
+                self._env.ale.getScreenGrayscale(self._obs_buffer[1])
+            if done:
+                self._env.reset()
+        self._lives = self._env.ale.lives()
+        obs = self._get_processed_obs()
+        return obs
 
     def step(self, action):
         """Applies the given action in the environment.
@@ -159,73 +111,40 @@ class AtariPreprocessing(object):
             execute fewer than self.frame_skip steps in the environment.
         * Furthermore, in this case the returned observation may not contain valid
             image data and should be ignored.
-        Args:
-            action: The action to be executed.
-        Returns:
-            observation: numpy array, the observation following the action.
-            reward: float, the reward following the action.
-            is_terminal: bool, whether the environment has reached a terminal state.
-                This is true when a life is lost and terminal_on_life_loss, or when the
-                episode is over.
-            info: Gym API's info data structure.
         """
-        accumulated_reward = 0.
+        accumulated_reward = 0.0
 
-        for time_step in range(self.frame_skip):
+        for i in range(self._frame_skip):
             # We bypass the Gym observation altogether and directly fetch the
             # grayscale image from the ALE. This is a little faster.
-            _, reward, game_over, info = self.environment.step(action)
+            _, reward, terminal, info = self._env.step(action)
             accumulated_reward += reward
 
-            # If we lose one life, reward - 1
-            new_lives = self.environment.ale.lives()
-            die = new_lives < self.lives
-            if die:
-                accumulated_reward -= 1
-            self.lives = new_lives
-
-            if self.terminal_on_life_loss:
-                is_terminal = game_over or die
-            else:
-                is_terminal = game_over
-
-            if is_terminal:
-                break
+            # Negetive reward on life loss.
+            lives = self._env.ale.lives()
+            die = (lives < self._lives)
+            self._lives = lives
+            if self._punish_on_loss and die:
+                accumulated_reward -= 1.0
             # We max-pool over the last two frames, in grayscale.
-            elif time_step >= self.frame_skip - 2:
-                t = time_step - (self.frame_skip - 2)
-                self._fetch_grayscale_observation(self.screen_buffer[t])
+            if i == self._frame_skip - 2:
+                self._env.ale.getScreenGrayscale(self._obs_buffer[0])
+            if i == self._frame_skip - 1:
+                self._env.ale.getScreenGrayscale(self._obs_buffer[1])
+            if terminal:
+                break
 
         # Pool the last two observations.
-        observation = self._pool_and_resize()
+        observation = self._get_processed_obs()
 
-        self.game_over = game_over
-        return observation, accumulated_reward, is_terminal, info
+        return observation, accumulated_reward, terminal, info
 
-    def _fetch_grayscale_observation(self, output):
-        """Returns the current observation in grayscale.
-        The returned observation is stored in 'output'.
-        Args:
-            output: numpy array, screen buffer to hold the returned observation.
-        Returns:
-            observation: numpy array, the current observation in grayscale.
-        """
-        self.environment.ale.getScreenGrayscale(output)
-        return output
+    def _get_processed_obs(self):
+        # For efficiency, done in-place.
+        np.maximum(self._obs_buffer[0], self._obs_buffer[1],
+                   out=self._obs_buffer[0])
 
-    def _pool_and_resize(self):
-        """Transforms two frames into a Nature DQN observation.
-        For efficiency, the transformation is done in-place in self.screen_buffer.
-        Returns:
-            transformed_screen: numpy array, pooled, resized screen.
-        """
-        # Pool if there are enough screens to do so.
-        if self.frame_skip > 1:
-            np.maximum(self.screen_buffer[0], self.screen_buffer[1],
-                       out=self.screen_buffer[0])
-
-        transformed_image = cv2.resize(self.screen_buffer[0],
-                                       (self.screen_size, self.screen_size),
-                                       interpolation=cv2.INTER_AREA)
-        int_image = np.asarray(transformed_image, dtype=np.uint8)
+        obs = cv2.resize(
+            self._obs_buffer[0], (self._width, self._height), interpolation=cv2.INTER_AREA)
+        int_image = np.asarray(obs, dtype=np.uint8)
         return int_image
