02751c0304c0694ffc0a8d18fc1ac3bc6f04cd04
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-03-16 03:24:38 +0000

    Use args from command.

--- run_pg_mujoco.sh
+++ run_pg_mujoco.sh
@@ -1,6 +1,6 @@
 # /bin/zsh
-ALGO=PPO2
-DIR_NAME=PPO2-baselines2
+ALGO=$1
+DIR_NAME=$2
 
 CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --exp_name ${ALGO} --allow_eval --dir_name ${DIR_NAME} > /dev/null &
 sleep 2
