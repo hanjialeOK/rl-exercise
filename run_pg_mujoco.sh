7e7254a7e0d8cbd6696e887a1444ab0cda1b20ff
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-03-16 15:18:32 +0000

    Sleep 1s.

--- run_pg_mujoco.sh
+++ run_pg_mujoco.sh
@@ -3,14 +3,14 @@ ALGO=$1
 DIR_NAME=$2
 
 CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --exp_name ${ALGO} --allow_eval --dir_name ${DIR_NAME} > /dev/null &
-sleep 2
+sleep 1
 CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --exp_name ${ALGO} --allow_eval --dir_name ${DIR_NAME} > /dev/null &
-sleep 2
+sleep 1
 CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --exp_name ${ALGO} --allow_eval --dir_name ${DIR_NAME} > /dev/null &
-sleep 2
+sleep 1
 CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --exp_name ${ALGO} --allow_eval --dir_name ${DIR_NAME} > /dev/null &
-sleep 2
+sleep 1
 CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --exp_name ${ALGO} --allow_eval --dir_name ${DIR_NAME} > /dev/null &
-sleep 2
+sleep 1
 CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --exp_name ${ALGO} --allow_eval --dir_name ${DIR_NAME} > /dev/null &
 echo 'Running six experiments...'
