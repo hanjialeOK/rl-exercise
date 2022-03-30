e1fb79ae42fd46dbb937de49679909d506ce884f
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-03-30 01:54:45 +0000

    Run six experiments with script.

---
+++ run_deepq_atari.sh
@@ -0,0 +1,17 @@
+# /bin/zsh
+ALGO=$1
+ENV=$2
+DIR_NAME=$3
+
+CUDA_VISIBLE_DEVICES=0 python run_deepq_atari.py --alg ${ALGO} --env ${ENV}  --dir_name ${DIR_NAME} > /dev/null &
+sleep 5
+CUDA_VISIBLE_DEVICES=0 python run_deepq_atari.py --alg ${ALGO} --env ${ENV}  --dir_name ${DIR_NAME} > /dev/null &
+sleep 5
+CUDA_VISIBLE_DEVICES=0 python run_deepq_atari.py --alg ${ALGO} --env ${ENV}  --dir_name ${DIR_NAME} > /dev/null &
+sleep 5
+CUDA_VISIBLE_DEVICES=1 python run_deepq_atari.py --alg ${ALGO} --env ${ENV}  --dir_name ${DIR_NAME} > /dev/null &
+sleep 5
+CUDA_VISIBLE_DEVICES=1 python run_deepq_atari.py --alg ${ALGO} --env ${ENV}  --dir_name ${DIR_NAME} > /dev/null &
+sleep 5
+CUDA_VISIBLE_DEVICES=1 python run_deepq_atari.py --alg ${ALGO} --env ${ENV}  --dir_name ${DIR_NAME} > /dev/null &
+echo 'Running six experiments...'
