9e056c4dcc83c310f013bb3f94656644cab483d4
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-04-02 00:54:30 +0000

    Run all envs in one command!

---
+++ run_pg_mujoco_all.sh
@@ -0,0 +1,26 @@
+# /bin/zsh
+ALGO=$1
+DIR_NAME=$2
+ENV=('Ant-v2' 'HalfCheetah-v2' 'Hopper-v2' 'Humanoid-v2' 'HumanoidStandup-v2'
+     'InvertedDoublePendulum-v2' 'InvertedPendulum-v2' 'Reacher-v2' 'Swimmer-v2' 'Walker2d-v2')
+LEN=${#ENV[*]}
+
+for i in $(seq 1 ${LEN})
+do
+    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    sleep 5
+    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    sleep 5
+    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    sleep 5
+    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    sleep 5
+    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    sleep 5
+    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    echo "Running ${ENV[i]} (${i}/${LEN}) for six experiments..."
+    if [ ${i} -lt ${LEN} ]; then
+        sleep 1h
+        echo "Sleeping!"
+    fi
+done
