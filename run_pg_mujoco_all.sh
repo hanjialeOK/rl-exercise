fb8b7be20268eb85d4814da083a86ad4b6912a65
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-04-03 13:15:02 +0000

    More efficient.

--- run_pg_mujoco_all.sh
+++ run_pg_mujoco_all.sh
@@ -1,4 +1,4 @@
-# /bin/zsh
+#!/bin/zsh
 ALGO=$1
 DIR_NAME=$2
 ENV=('Ant-v2' 'HalfCheetah-v2' 'Hopper-v2' 'Humanoid-v2' 'HumanoidStandup-v2'
@@ -7,20 +7,35 @@ LEN=${#ENV[*]}
 
 for i in $(seq 1 ${LEN})
 do
-    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py \
+        --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
     sleep 5
-    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py \
+        --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
     sleep 5
-    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py \
+        --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
     sleep 5
-    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py \
+        --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
     sleep 5
-    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py \
+        --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
     sleep 5
-    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
+    CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py \
+        --alg ${ALGO} --env ${ENV[i]} --dir_name ${DIR_NAME} > /dev/null &
     echo "Running ${ENV[i]} (${i}/${LEN}) for six experiments..."
-    if [ ${i} -lt ${LEN} ]; then
-        sleep 1h
-        echo "Sleeping!"
-    fi
+    sleep 30m
+    while true
+    do
+        pid0=$(fuser /dev/nvidia0)
+        pid1=$(fuser /dev/nvidia1)
+        if [ ! ${pid0} ] && [ ! ${pid1} ]; then
+            echo "Completed!"
+            break
+        else
+            echo "Busy! ${pid0}${pid1} is running."
+            sleep 1m
+        fi
+    done
 done
