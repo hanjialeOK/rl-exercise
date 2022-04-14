dde6d86196085412786b112b594aabcde4d4874c
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-04-14 03:47:03 +0000

    Rename file: vpgbuffer.py -> gaebuffer.py.

--- pg/buffer/gaebuffer.py
+++ pg/buffer/gaebuffer.py
@@ -4,64 +4,7 @@ import numpy as np
 EPS = 1e-8
 
 
-class VPGBuffer:
-    def __init__(self, obs_dim, act_dim, size, num_env=1, gamma=0.99, lam=0.95):
-        self.obs_buf = np.zeros((size, num_env) + obs_dim, dtype=np.float64)
-        self.act_buf = np.zeros((size, num_env) + act_dim, dtype=np.float32)
-        self.adv_buf = np.zeros((size, num_env), dtype=np.float32)
-        self.rew_buf = np.zeros((size, num_env), dtype=np.float32)
-        self.done_buf = np.zeros((size, num_env), dtype=np.float32)
-        self.ret_buf = np.zeros((size, num_env), dtype=np.float32)
-        self.val_buf = np.zeros((size, num_env), dtype=np.float32)
-        self.gamma, self.lam = gamma, lam
-        self.ptr, self.max_size = 0, size
-        self.obs_dim = obs_dim
-        self.act_dim = act_dim
-        self.num_env = num_env
-
-    def store(self, obs, act, rew, done, val):
-        """
-        Store transition.
-        """
-        assert self.ptr < self.max_size
-        assert obs.shape == (self.num_env,) + self.obs_dim
-        assert act.shape == (self.num_env,) + self.act_dim
-        assert rew.shape == (self.num_env,)
-        assert done.shape == (self.num_env,)
-        assert val.shape == (self.num_env,)
-        self.obs_buf[self.ptr] = obs
-        self.act_buf[self.ptr] = act
-        self.rew_buf[self.ptr] = rew
-        self.done_buf[self.ptr] = done
-        self.val_buf[self.ptr] = val
-        self.ptr += 1
-
-    def finish_path(self, last_val=None):
-        assert self.ptr == self.max_size
-        assert last_val.shape == (self.num_env,)
-        vals = np.append(self.val_buf, last_val.reshape(1, -1), axis=0)
-
-        # the next two" lines implement GAE-Lambda advantage calculation
-        lastgaelam = 0.0
-        for t in reversed(range(self.ptr)):
-            nondone = 1.0 - self.done_buf[t]
-            delta = self.rew_buf[t] + self.gamma * \
-                nondone * vals[t + 1] - vals[t]
-            self.adv_buf[t] = lastgaelam = delta + \
-                self.gamma * self.lam * nondone * lastgaelam
-        self.ret_buf = self.adv_buf + self.val_buf
-        pass
-
-    def get(self):
-        assert self.ptr == self.max_size
-        self.ptr = 0
-        return [self.obs_buf.reshape(self.max_size * self.num_env, -1),
-                self.act_buf.reshape(self.max_size * self.num_env, -1),
-                self.adv_buf.reshape(-1),
-                self.ret_buf.reshape(-1)]
-
-
-class PPOBuffer:
+class GAEBuffer:
     def __init__(self, obs_dim, act_dim, size, num_env=1,  gamma=0.99, lam=0.95):
         self.obs_buf = np.zeros((size, num_env) + obs_dim, dtype=np.float64)
         self.act_buf = np.zeros((size, num_env) + act_dim, dtype=np.float32)
@@ -78,9 +21,6 @@ class PPOBuffer:
         self.num_env = num_env
 
     def store(self, obs, act, rew, done, val, logp):
-        """
-        Store transition.
-        """
         assert self.ptr < self.max_size
         assert obs.shape == (self.num_env,) + self.obs_dim
         assert act.shape == (self.num_env,) + self.act_dim
@@ -101,7 +41,7 @@ class PPOBuffer:
         assert last_val.shape == (self.num_env,)
         vals = np.append(self.val_buf, last_val.reshape(1, -1), axis=0)
 
-        # the next two" lines implement GAE-Lambda advantage calculation
+        # GAE-Lambda advantage calculation
         lastgaelam = 0.0
         for t in reversed(range(self.ptr)):
             nondone = 1.0 - self.done_buf[t]
@@ -260,57 +200,31 @@ class PPOTD0Buffer:
                 self.val_buf.reshape(-1)]
 
 
-class TRPOBuffer:
-    def __init__(self, obs_dim, act_dim, size, gamma=0.99, lam=0.95):
-        self.obs_buf = np.zeros((size, ) + obs_dim, dtype=np.float32)
-        self.act_buf = np.zeros((size, ) + act_dim, dtype=np.float32)
-        self.adv_buf = np.zeros(size, dtype=np.float32)
-        self.rew_buf = np.zeros(size, dtype=np.float32)
-        self.done_buf = np.zeros(size, dtype=np.float32)
-        self.ret_buf = np.zeros(size, dtype=np.float32)
-        self.val_buf = np.zeros(size, dtype=np.float32)
-        self.logp_buf = np.zeros(size, dtype=np.float32)
-        self.mu_buf = np.zeros((size, ) + act_dim, dtype=np.float32)
-        self.logstd_buf = np.zeros((size, ) + act_dim, dtype=np.float32)
-        self.gamma, self.lam = gamma, lam
-        self.ptr, self.max_size = 0, size
+class TRPOBuffer(GAEBuffer):
+    def __init__(self, obs_dim, act_dim, size, num_env=1, gamma=0.99, lam=0.95):
+        self.mu_buf = np.zeros((size, num_env) + act_dim, dtype=np.float32)
+        self.logstd_buf = np.zeros((size, 1) + act_dim, dtype=np.float32)
+        super().__init__(obs_dim=obs_dim, act_dim=act_dim, size=size,
+                         num_env=num_env, gamma=gamma, lam=lam)
 
     def store(self, obs, act, rew, done, val, logp, mu, logstd):
-        """
-        Store transition.
-        """
-        assert self.ptr < self.max_size
-        self.obs_buf[self.ptr] = obs
-        self.act_buf[self.ptr] = act
-        self.rew_buf[self.ptr] = rew
-        self.done_buf[self.ptr] = done
-        self.val_buf[self.ptr] = val
-        self.logp_buf[self.ptr] = logp
+        assert mu.shape == (self.num_env,) + self.act_dim
+        assert logstd.shape == (1,) + self.act_dim
         self.mu_buf[self.ptr] = mu
         self.logstd_buf[self.ptr] = logstd
-        self.ptr += 1
+        super().store(obs, act, rew, done, val, logp)
 
-    def finish_path(self, last_val=0.):
-        assert self.ptr == self.max_size
-        rews = np.append(self.rew_buf, last_val)
-        vals = np.append(self.val_buf, last_val)
-
-        # the next two" lines implement GAE-Lambda advantage calculation
-        lastgaelam = 0.0
-        lastret = rews[-1]
-        for t in reversed(range(self.ptr)):
-            nondone = 1.0 - self.done_buf[t]
-            max_kl = rews[t] + self.gamma * nondone * vals[t + 1] - vals[t]
-            self.adv_buf[t] = lastgaelam = max_kl + \
-                self.gamma * self.lam * nondone * lastgaelam
-            self.ret_buf[t] = lastret = rews[t] + \
-                self.gamma * nondone * lastret
+    def finish_path(self, last_val=None):
+        super().finish_path(last_val=last_val)
 
     def get(self):
         assert self.ptr == self.max_size
         self.ptr = 0
-        # the next two lines implement the advantage normalization trick
-        self.adv_buf = (self.adv_buf - np.mean(self.adv_buf)) / \
-            (np.std(self.adv_buf) + EPS)
-        return [self.obs_buf, self.act_buf, self.adv_buf, self.ret_buf,
-                self.logp_buf, self.mu_buf, self.logstd_buf]
+        return [self.obs_buf.reshape(self.max_size * self.num_env, -1),
+                self.act_buf.reshape(self.max_size * self.num_env, -1),
+                self.adv_buf.reshape(-1),
+                self.ret_buf.reshape(-1),
+                self.logp_buf.reshape(-1),
+                self.val_buf.reshape(-1),
+                self.mu_buf.reshape(self.max_size * self.num_env, -1),
+                self.logstd_buf.reshape(self.max_size * self.num_env, -1)]
