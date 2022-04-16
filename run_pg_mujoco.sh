47832a86bdfae0a20111cce3059d77ffd55c1143
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-04-16 08:57:46 +0000

    Ignore warnings.

--- run_pg_mujoco.sh
+++ run_pg_mujoco.sh
@@ -4,21 +4,21 @@ ENV=$2
 DIR_NAME=$3
 NUM_ENV=$4
 
-CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py \
+echo "Running ${ALGO} of rl-exercise in ${ENV} for six experiments..."
+CUDA_VISIBLE_DEVICES=0 PYTHONWARNINGS=ignore python run_pg_mujoco.py \
     --alg ${ALGO} --env ${ENV} --dir_name ${DIR_NAME} --num_env ${NUM_ENV} > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py \
+CUDA_VISIBLE_DEVICES=0 PYTHONWARNINGS=ignore python run_pg_mujoco.py \
     --alg ${ALGO} --env ${ENV} --dir_name ${DIR_NAME} --num_env ${NUM_ENV} > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=0 python run_pg_mujoco.py \
+CUDA_VISIBLE_DEVICES=0 PYTHONWARNINGS=ignore python run_pg_mujoco.py \
     --alg ${ALGO} --env ${ENV} --dir_name ${DIR_NAME} --num_env ${NUM_ENV} > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py \
+CUDA_VISIBLE_DEVICES=1 PYTHONWARNINGS=ignore python run_pg_mujoco.py \
     --alg ${ALGO} --env ${ENV} --dir_name ${DIR_NAME} --num_env ${NUM_ENV} > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py \
+CUDA_VISIBLE_DEVICES=1 PYTHONWARNINGS=ignore python run_pg_mujoco.py \
     --alg ${ALGO} --env ${ENV} --dir_name ${DIR_NAME} --num_env ${NUM_ENV} > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=1 python run_pg_mujoco.py \
+CUDA_VISIBLE_DEVICES=1 PYTHONWARNINGS=ignore python run_pg_mujoco.py \
     --alg ${ALGO} --env ${ENV} --dir_name ${DIR_NAME} --num_env ${NUM_ENV} > /dev/null &
-echo 'Running six experiments...'
