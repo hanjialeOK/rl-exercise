8713d4436e13cd1d2b584d4be12b95eb27f3fbdf
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-04-17 03:50:49 +0000

    Refactor code.

--- pg/agents/vpg.py
+++ pg/agents/vpg.py
@@ -65,15 +65,17 @@ class VPGAgent(BaseAgent):
     """
 
     def __init__(self, sess, obs_dim, act_dim, num_env=1,
-                 lr=3e-4, train_iters=10, ent_coef=0.0,
-                 vf_coef=0.5, max_grad_norm=0.5, horizon=2048,
+                 pi_lr=1e-3, vf_lr=1e-3, target_kl=0.01, train_iters=5,
+                 ent_coef=0.0, vf_coef=0.5, max_grad_norm=0.5, horizon=2048,
                  minibatch=64, gamma=0.99, lam=0.95,
                  fixed_lr=True):
         self.sess = sess
         self.obs_dim = obs_dim
         self.act_dim = act_dim
         self.num_env = num_env
-        self.lr = lr
+        self.pi_lr = pi_lr
+        self.vf_lr = vf_lr
+        self.target_kl = target_kl
         self.train_iters = train_iters
         self.horizon = horizon
         self.minibatch = minibatch * num_env
@@ -93,24 +95,22 @@ class VPGAgent(BaseAgent):
         self.critic = CriticMLP(name='vf')
 
     def _build_train_op(self):
-        ob1_ph = tf.compat.v1.placeholder(
+        self.ob1_ph = ob1_ph = tf.compat.v1.placeholder(
             shape=(self.num_env, ) + self.obs_dim, dtype=tf.float32, name="ob1_ph")
-        obs_ph = tf.compat.v1.placeholder(
-            shape=(self.minibatch, ) + self.obs_dim, dtype=tf.float32, name="obs_ph")
-        act_ph = tf.compat.v1.placeholder(
-            shape=(self.minibatch, ) + self.act_dim, dtype=tf.float32, name="act_ph")
-        adv_ph = tf.compat.v1.placeholder(
-            shape=[self.minibatch, ], dtype=tf.float32, name="adv_ph")
-        ret_ph = tf.compat.v1.placeholder(
-            shape=[self.minibatch, ], dtype=tf.float32, name="ret_ph")
-        logp_old_ph = tf.compat.v1.placeholder(
-            shape=[self.minibatch, ], dtype=tf.float32, name="logp_old_ph")
-        val_ph = tf.compat.v1.placeholder(
-            shape=[self.minibatch, ], dtype=tf.float32, name="val_ph")
-        lr_ph = tf.compat.v1.placeholder(
-            shape=[], dtype=tf.float32, name="lr_ph")
-
-        all_phs = [obs_ph, act_ph, adv_ph, ret_ph, logp_old_ph, val_ph, lr_ph]
+        self.obs_ph = obs_ph = tf.compat.v1.placeholder(
+            shape=(None, ) + self.obs_dim, dtype=tf.float32, name="obs_ph")
+        self.act_ph = act_ph = tf.compat.v1.placeholder(
+            shape=(None, ) + self.act_dim, dtype=tf.float32, name="act_ph")
+        self.adv_ph = adv_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="adv_ph")
+        self.ret_ph = ret_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="ret_ph")
+        self.logp_old_ph = logp_old_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="logp_old_ph")
+        self.val_ph = val_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="val_ph")
+        self.pi_lr_ph = pi_lr_ph = tf.compat.v1.placeholder(
+            shape=[], dtype=tf.float32, name="pi_lr_ph")
 
         # Probability distribution
         logstd = tf.compat.v1.get_variable(
@@ -142,21 +142,16 @@ class VPGAgent(BaseAgent):
         # Usefull Infos
         approx_kl = 0.5 * tf.reduce_mean(tf.square(logp_old_ph - logp_a))
 
-        # Total loss
-        loss = pi_loss - meanent * self.ent_coef + vf_loss * self.vf_coef
-
         # Optimizers
         pi_optimizer = tf.train.AdamOptimizer(
-            learning_rate=lr_ph, epsilon=1e-5)
+            learning_rate=pi_lr_ph, epsilon=1e-8)
         vf_optimizer = tf.train.AdamOptimizer(
-            learning_rate=lr_ph, epsilon=1e-5)
+            learning_rate=self.vf_lr, epsilon=1e-8)
         pi_params = self._get_var_list('pi')
         vf_params = self._get_var_list('vf')
         pi_train_op = pi_optimizer.minimize(pi_loss, var_list=pi_params)
         vf_train_op = vf_optimizer.minimize(vf_loss, var_list=vf_params)
 
-        self.ob1_ph = ob1_ph
-        self.all_phs = all_phs
         self.get_action_ops = get_action_ops
         self.v1 = v1
         self.pi_loss = pi_loss
@@ -166,16 +161,45 @@ class VPGAgent(BaseAgent):
         self.pi_train_op = pi_train_op
         self.vf_train_op = vf_train_op
 
-    def update(self, frac):
+    def update(self, frac=None):
         buf_data = self.buffer.get()
         assert buf_data[0].shape[0] == self.horizon * self.num_env
+        [obs, actions, advs, rets, logprobs, values] = buf_data
+        advs = (advs - np.mean(advs)) / (np.std(advs) + 1e-8)
+
+        indices = np.arange(self.horizon * self.num_env)
+        np.random.shuffle(indices)
+
+        pi_inputs = {
+            self.obs_ph: obs[indices],
+            self.act_ph: actions[indices],
+            self.adv_ph: advs[indices],
+            self.logp_old_ph: logprobs[indices],
+            self.pi_lr_ph: self.pi_lr,
+        }
 
         pi_loss_buf = []
         vf_loss_buf = []
         entropy_buf = []
         kl_buf = []
+        lr_buf = []
 
-        indices = np.arange(self.horizon * self.num_env)
+        pi_loss, entropy, klold, _ = self.sess.run(
+            [self.pi_loss, self.entropy,
+             self.approx_kl, self.pi_train_op],
+            feed_dict=pi_inputs)
+        klnew = self.sess.run(
+            self.approx_kl, feed_dict=pi_inputs)
+        pi_loss_buf.append(pi_loss)
+        entropy_buf.append(entropy)
+        kl_buf.append(klnew)
+        lr_buf.append(self.pi_lr)
+
+        if klnew > self.target_kl * 1.5:
+            self.pi_lr /= 1.5
+        elif klnew < self.target_kl / 1.5:
+            self.pi_lr *= 1.5
+        # self.pi_lr = np.clip(self.pi_lr, 0, 1e-3)
 
         for i in range(self.train_iters):
             # Randomize the indexes
@@ -184,37 +208,19 @@ class VPGAgent(BaseAgent):
             for start in range(0, self.horizon, self.minibatch):
                 end = start + self.minibatch
                 mbinds = indices[start:end]
-                slices = [arr[mbinds] for arr in buf_data]
-                [obs, actions, advs, rets, logprobs, values] = slices
-                # advs_raw = rets - values
-                advs = (advs - np.mean(advs)) / (np.std(advs) + 1e-8)
-                lr = self.lr if self.fixed_lr else self.lr * frac
-                inputs = {
-                    self.all_phs[0]: obs,
-                    self.all_phs[1]: actions,
-                    self.all_phs[2]: advs,
-                    self.all_phs[3]: rets,
-                    self.all_phs[4]: logprobs,
-                    self.all_phs[5]: values,
-                    self.all_phs[6]: lr,
+                vf_inputs = {
+                    self.obs_ph: obs[mbinds],
+                    self.ret_ph: rets[mbinds]
                 }
-
-                if i == 1:
-                    pi_loss, entropy, kl, _ = self.sess.run(
-                        [self.pi_loss, self.entropy, self.approx_kl,
-                         self.pi_train_op],
-                        feed_dict=inputs)
-                    pi_loss_buf.append(pi_loss)
-                    entropy_buf.append(entropy)
-                    kl_buf.append(kl)
 
                 vf_loss, _ = self.sess.run(
                     [self.vf_loss, self.vf_train_op],
-                    feed_dict=inputs)
+                    feed_dict=vf_inputs)
                 vf_loss_buf.append(vf_loss)
 
         return [np.mean(pi_loss_buf), np.mean(vf_loss_buf),
-                np.mean(entropy_buf), np.mean(kl_buf)]
+                np.mean(entropy_buf), np.mean(kl_buf),
+                np.mean(lr_buf)]
 
     def select_action(self, obs, deterministic=False):
         [mu, pi, v, logp_pi] = self.sess.run(
