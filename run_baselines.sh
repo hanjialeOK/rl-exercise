92ad314dd997353b1ac54c53d16f9dd800218291
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-04-17 07:00:25 +0000

    Ignore warnnings.

--- run_baselines.sh
+++ run_baselines.sh
@@ -6,32 +6,33 @@ ENV=$2
 DIR_NAME=$3
 NUM_ENV=$4
 
-CUDA_VISIBLE_DEVICES=0 python -m baselines.run \
+echo "Running ${ALGO} of baselines in ${ENV} for six experiments..."
+CUDA_VISIBLE_DEVICES=0 PYTHONWARNINGS=ignore python -m baselines.run \
     --alg=${ALGO} --env=${ENV} --num_timesteps=1e6 --num_env=${NUM_ENV}\
     --log_path=/data/hanjl/my_results/${ENV}/${DIR_NAME}/seed0 \
     > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=0 python -m baselines.run \
+CUDA_VISIBLE_DEVICES=0 PYTHONWARNINGS=ignore python -m baselines.run \
     --alg=${ALGO} --env=${ENV} --num_timesteps=1e6 --num_env=${NUM_ENV}\
     --log_path=/data/hanjl/my_results/${ENV}/${DIR_NAME}/seed1 \
     > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=0 python -m baselines.run \
+CUDA_VISIBLE_DEVICES=0 PYTHONWARNINGS=ignore python -m baselines.run \
     --alg=${ALGO} --env=${ENV} --num_timesteps=1e6 --num_env=${NUM_ENV}\
     --log_path=/data/hanjl/my_results/${ENV}/${DIR_NAME}/seed2 \
     > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=1 python -m baselines.run \
+CUDA_VISIBLE_DEVICES=1 PYTHONWARNINGS=ignore python -m baselines.run \
     --alg=${ALGO} --env=${ENV} --num_timesteps=1e6 --num_env=${NUM_ENV}\
     --log_path=/data/hanjl/my_results/${ENV}/${DIR_NAME}/seed3 \
     > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=1 python -m baselines.run \
+CUDA_VISIBLE_DEVICES=1 PYTHONWARNINGS=ignore python -m baselines.run \
     --alg=${ALGO} --env=${ENV} --num_timesteps=1e6 --num_env=${NUM_ENV}\
     --log_path=/data/hanjl/my_results/${ENV}/${DIR_NAME}/seed4 \
     > /dev/null &
 sleep 5
-CUDA_VISIBLE_DEVICES=1 python -m baselines.run \
+CUDA_VISIBLE_DEVICES=1 PYTHONWARNINGS=ignore python -m baselines.run \
     --alg=${ALGO} --env=${ENV} --num_timesteps=1e6 --num_env=${NUM_ENV}\
     --log_path=/data/hanjl/my_results/${ENV}/${DIR_NAME}/seed5 \
     > /dev/null &
