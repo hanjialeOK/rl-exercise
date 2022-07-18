9c103f2b066b14a4355d3b780baf5881e5e3229d
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-07-18 07:57:18 +0000

    Adapted a little based on baselines.

--- common/vec_env/vec_normalize.py
+++ common/vec_env/vec_normalize.py
@@ -8,58 +8,53 @@ class VecNormalize(VecEnvWrapper):
     and returns from an environment.
     """
 
-    def __init__(self, venv, ob=True, ret=True, clipob=10., cliprew=10.,
-                 gamma=0.99, epsilon=1e-8):
+    def __init__(self, venv, ob=True, ret=True, clipob=10., cliprew=10., gamma=0.99, epsilon=1e-8):
         VecEnvWrapper.__init__(self, venv)
         from common.running_mean_std import RunningMeanStd
-        self.ob_rms = RunningMeanStd(
-            shape=self.observation_space.shape) if ob else None
+        self.ob_rms = RunningMeanStd(shape=self.observation_space.shape) if ob else None
         self.ret_rms = RunningMeanStd(shape=()) if ret else None
         self.clipob = clipob
         self.cliprew = cliprew
-        # self.ret = np.zeros(self.num_envs)
+        self.ret = np.zeros(self.num_envs)
         self.gamma = gamma
         self.epsilon = epsilon
 
     def step_wait(self):
         obs, rews, news, infos = self.venv.step_wait()
-        # self.ret = self.ret * self.gamma + rews
-        obs = self._obfilt(obs)
+        self.raw_obs = obs.copy()
+        self.raw_rews = rews.copy()
+        self.ret = self.ret * self.gamma + rews
+        if self.ob_rms:
+            self.ob_rms.update(obs)
+            obs = self._obfilt(obs)
         if self.ret_rms:
-            # self.ret_rms.update(self.ret)
-            rews = np.clip(rews / np.sqrt(self.ret_rms.var +
-                           self.epsilon), -self.cliprew, self.cliprew)
-        # self.ret[news] = 0.
+            self.ret_rms.update(self.ret)
+            rews = self._rewfilt(rews)
+        self.ret[news] = 0.
         return obs, rews, news, infos
 
     def _obfilt(self, obs):
         if self.ob_rms:
-            # self.ob_rms.update(obs)
-            obs = np.clip((obs - self.ob_rms.mean) / np.sqrt(self.ob_rms.var +
-                          self.epsilon), -self.clipob, self.clipob)
+            obs = np.clip((obs - self.ob_rms.mean) / np.sqrt(self.ob_rms.var + self.epsilon),
+                          -self.clipob, self.clipob)
             return obs
         else:
             return obs
 
+    def _rewfilt(self, rews):
+        if self.ret_rms:
+            rews = np.clip(rews / np.sqrt(self.ret_rms.var + self.epsilon),
+                           -self.cliprew, self.cliprew)
+            return rews
+        else:
+            return rews
+
+    def get_raw(self):
+        return self.raw_obs, self.raw_rews
+
     def reset(self):
-        # self.ret = np.zeros(self.num_envs)
+        self.ret = np.zeros(self.num_envs)
         obs = self.venv.reset()
+        self.raw_obs = obs.copy()
+        self.raw_rews = np.zeros(self.num_envs)
         return self._obfilt(obs)
-
-    def update_rms(self, obs, ret):
-        if self.ob_rms:
-            obs_raw = self._unnormalize_obs(obs)
-            self.ob_rms.update(obs_raw)
-        if self.ret_rms:
-            ret_raw = self._unnormalize_ret(ret)
-            self.ret_rms.update(ret_raw)
-
-    def _unnormalize_obs(self, obs):
-        if self.ob_rms:
-            return (obs * np.sqrt(self.ob_rms.var + self.epsilon)) + self.ob_rms.mean
-        return obs
-
-    def _unnormalize_ret(self, ret):
-        if self.ret_rms:
-            return ret * np.sqrt(self.ret_rms.var + self.epsilon)
-        return ret
