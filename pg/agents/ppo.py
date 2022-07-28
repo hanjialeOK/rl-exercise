ba40f86d883097fdee43ecc0db11e6d2f8e6b2cf
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-07-28 06:16:08 +0000

    Add ppo penalty.

---
+++ pg/agents/ppo.py
@@ -0,0 +1,303 @@
+import numpy as np
+import tensorflow as tf
+
+import pg.buffer.gaebuffer as Buffer
+from pg.agents.base import BaseAgent
+from common.distributions import DiagGaussianPd
+
+
+def tf_ortho_init(scale):
+    return tf.keras.initializers.Orthogonal(scale)
+
+
+class ActorMLP(tf.keras.Model):
+    def __init__(self, ac_dim, name=None):
+        super(ActorMLP, self).__init__(name=name)
+        activation_fn = tf.keras.activations.tanh
+        kernel_initializer = None
+        self.dense1 = tf.keras.layers.Dense(
+            64, activation=activation_fn,
+            kernel_initializer=tf_ortho_init(np.sqrt(2)), name='fc1')
+        self.dense2 = tf.keras.layers.Dense(
+            64, activation=activation_fn,
+            kernel_initializer=tf_ortho_init(np.sqrt(2)), name='fc2')
+        self.dense3 = tf.keras.layers.Dense(
+            ac_dim[0], activation=None,
+            kernel_initializer=tf_ortho_init(0.01), name='fc3')
+
+    def call(self, state):
+        x = tf.cast(state, tf.float32)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        return x
+
+
+class CriticMLP(tf.keras.Model):
+    def __init__(self, name=None):
+        super(CriticMLP, self).__init__(name=name)
+        activation_fn = tf.keras.activations.tanh
+        kernel_initializer = None
+        self.dense1 = tf.keras.layers.Dense(
+            64, activation=activation_fn,
+            kernel_initializer=tf_ortho_init(np.sqrt(2)), name='fc1')
+        self.dense2 = tf.keras.layers.Dense(
+            64, activation=activation_fn,
+            kernel_initializer=tf_ortho_init(np.sqrt(2)), name='fc2')
+        self.dense3 = tf.keras.layers.Dense(
+            1, activation=None,
+            kernel_initializer=tf_ortho_init(1.0), name='fc3')
+
+    def call(self, state):
+        x = tf.cast(state, tf.float32)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        return tf.squeeze(x, axis=1)
+
+
+class PPOAgent(BaseAgent):
+    def __init__(self, sess, env,
+                 clip_ratio=0.2, lr=3e-4, train_iters=10, target_kl=0.01,
+                 ent_coef=0.0, vf_coef=0.5, max_grad_norm=0.5,
+                 horizon=2048, minibatch=64, gamma=0.99, lam=0.95,
+                 grad_clip=True, vf_clip=True, fixed_lr=False,
+                 beta=1):
+        self.sess = sess
+        self.obs_shape = env.observation_space.shape
+        self.ac_shape = env.action_space.shape
+        self.clip_ratio = clip_ratio
+        self.lr = lr
+        self.train_iters = train_iters
+        self.target_kl = target_kl
+        self.horizon = horizon
+        self.minibatch = minibatch
+        self.ent_coef = ent_coef
+        self.vf_coef = vf_coef
+        self.max_grad_norm = max_grad_norm
+        self.grad_clip = grad_clip
+        self.vf_clip = vf_clip
+        self.fixed_lr = fixed_lr
+        self.beta = beta
+
+        self.buffer = Buffer.GAEBuffer(
+            env, horizon=horizon, gamma=gamma, lam=lam,
+            compute_v=self.compute_v)
+        self._build_network()
+        self._build_train_op()
+        self.sync_op = self._build_sync_op()
+        super().__init__()
+
+    def _build_network(self):
+        self.actor = ActorMLP(self.ac_shape, name='pi')
+        self.actor_pik = ActorMLP(self.ac_shape, name='pik')
+        self.critic = CriticMLP(name='vf')
+
+    def _build_train_op(self):
+        self.ob1_ph = ob1_ph = tf.compat.v1.placeholder(
+            shape=(1, ) + self.obs_shape, dtype=tf.float32, name="ob1_ph")
+        self.obs_ph = obs_ph = tf.compat.v1.placeholder(
+            shape=(None, ) + self.obs_shape, dtype=tf.float32, name="obs_ph")
+        self.ac_ph = ac_ph = tf.compat.v1.placeholder(
+            shape=(None, ) + self.ac_shape, dtype=tf.float32, name="ac_ph")
+        self.adv_ph = adv_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="adv_ph")
+        self.ret_ph = ret_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="ret_ph")
+        self.neglogp_old_ph = neglogp_old_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="neglogp_old_ph")
+        self.val_ph = val_ph = tf.compat.v1.placeholder(
+            shape=[None, ], dtype=tf.float32, name="val_ph")
+        self.lr_ph = lr_ph = tf.compat.v1.placeholder(
+            shape=[], dtype=tf.float32, name="lr_ph")
+        self.beta_ph = beta_ph = tf.compat.v1.placeholder(
+            shape=[], dtype=tf.float32, name="beta_ph")
+
+        # Probability distribution
+        logstd1 = tf.compat.v1.get_variable(
+            name='pi/logstd', shape=(1, self.ac_shape[0]),
+            initializer=tf.zeros_initializer)
+        logstd1_pik = tf.compat.v1.get_variable(
+            name='pik/logstd_pik', shape=(1, self.ac_shape[0]),
+            initializer=tf.zeros_initializer)
+
+        # Interative with env
+        mu1 = self.actor(ob1_ph)
+        dist1 = DiagGaussianPd(mu1, logstd1)
+        pi1 = dist1.sample()
+        neglogp1 = tf.reduce_sum(dist1.neglogp(pi1), axis=1)
+
+        v1 = self.critic(ob1_ph)
+
+        get_action_ops = [mu1, pi1, v1, neglogp1]
+
+        # Train batch data
+        mu = self.actor(obs_ph)
+        logstd = tf.tile(logstd1, (tf.shape(mu)[0], 1))
+        dist = DiagGaussianPd(mu, logstd)
+        neglogpac = tf.reduce_sum(dist.neglogp(ac_ph), axis=1)
+        entropy = tf.reduce_sum(dist.entropy(), axis=1)
+        meanent = tf.reduce_mean(entropy)
+
+        mu_pik = tf.stop_gradient(self.actor_pik(obs_ph))
+        logstd_pik = tf.tile(logstd1_pik, (tf.shape(mu_pik)[0], 1))
+        dist_pik = DiagGaussianPd(mu_pik, logstd_pik)
+        kl = tf.reduce_sum(dist.kl(dist_pik), axis=1)
+        meankl = tf.reduce_mean(kl)
+
+        v = self.critic(obs_ph)
+
+        if self.vf_clip:
+            valclipped = val_ph + \
+                tf.clip_by_value(v - val_ph, -self.clip_ratio, self.clip_ratio)
+            vf_loss1 = tf.square(v - ret_ph)
+            vf_loss2 = tf.square(valclipped - ret_ph)
+            vf_loss = 0.5 * tf.reduce_mean(tf.maximum(vf_loss1, vf_loss2))
+        else:
+            vf_loss = 0.5 * tf.reduce_mean(tf.square(v - ret_ph))
+
+        # PPO objectives
+        # pi(a|s) / pi_old(a|s), should be one at the first iteration
+        ratio = tf.exp(neglogp_old_ph - neglogpac)
+        pi_loss1 = -adv_ph * ratio
+        pi_loss2 = -adv_ph * tf.clip_by_value(
+            ratio, 1.0 - self.clip_ratio, 1.0 + self.clip_ratio)
+        pi_loss = tf.reduce_mean(tf.maximum(pi_loss1, pi_loss2))
+
+        kl_penalty = meankl
+        pi_loss += beta_ph * kl_penalty
+
+        # Total loss
+        loss = pi_loss - meanent * self.ent_coef + vf_loss * self.vf_coef
+
+        # Info (useful to watch during learning)
+        # a sample estimate for KL-divergence, easy to compute
+        approxkl = 0.5 * tf.reduce_mean(tf.square(neglogp_old_ph - neglogpac))
+        absratio = tf.reduce_mean(tf.abs(ratio - 1.0) + 1.0)
+        ratioclipped = tf.where(
+            adv_ph > 0,
+            ratio > (1.0 + self.clip_ratio),
+            ratio < (1.0 - self.clip_ratio))
+        ratioclipfrac = tf.reduce_mean(tf.cast(ratioclipped, tf.float32))
+
+        # Optimizers
+        optimizer = tf.compat.v1.train.AdamOptimizer(
+            learning_rate=lr_ph, epsilon=1e-5)
+        params = self._get_var_list('pi') + self._get_var_list('vf')
+        grads_and_vars = optimizer.compute_gradients(loss, var_list=params)
+        grads, vars = zip(*grads_and_vars)
+        if self.grad_clip:
+            grads, _grad_norm = tf.clip_by_global_norm(
+                grads, self.max_grad_norm)
+        gradclipped = _grad_norm > self.max_grad_norm if self.grad_clip else tf.zeros(())
+        grads_and_vars = list(zip(grads, vars))
+
+        train_op = optimizer.apply_gradients(grads_and_vars)
+
+        self.get_action_ops = get_action_ops
+        self.v1 = v1
+        self.absratio = absratio
+        self.pi_loss = pi_loss
+        self.vf_loss = vf_loss
+        self.meankl = meankl
+        self.meanent = meanent
+        self.ratioclipfrac = ratioclipfrac
+        self.gradclipped = gradclipped
+        self.train_op = train_op
+
+        self.losses = [kl_penalty, pi_loss, vf_loss, meanent, meankl]
+        self.infos = [absratio, ratioclipfrac, gradclipped]
+
+    def _build_sync_op(self):
+        sync_qt_ops = []
+        pi_params = self._get_var_list('pi')
+        pi_params_old = self._get_var_list('pik')
+
+        for (newv, oldv) in zip(pi_params, pi_params_old):
+            sync_qt_ops.append(oldv.assign(newv, use_locking=True))
+        return sync_qt_ops
+
+    def update(self, frac, logger):
+        buf_data = self.buffer.get()
+        [obs_all, ac_all, adv_all, ret_all, val_all, neglogp_all] = buf_data
+        assert obs_all.shape[0] == self.horizon
+
+        lr = self.lr if self.fixed_lr else self.lr * frac
+        # lr = self.lr if self.fixed_lr else np.maximum(self.lr * frac, 1e-4)
+
+        pi_loss_buf = []
+        vf_loss_buf = []
+        ent_buf = []
+        kl_buf = []
+        kl_penalty_buf = []
+        ratio_buf = []
+        ratioclipfrac_buf = []
+        gradclipped_buf = []
+
+        self.sess.run(self.sync_op)
+
+        indices = np.arange(self.horizon)
+        for _ in range(self.train_iters):
+            # Randomize the indexes
+            np.random.shuffle(indices)
+            # 0 to batch_size with batch_train_size step
+            for start in range(0, self.horizon, self.minibatch):
+                end = start + self.minibatch
+                mbinds = indices[start:end]
+                advs = adv_all[mbinds]
+                advs = (advs - np.mean(advs)) / (np.std(advs) + 1e-8)
+                inputs = {
+                    self.obs_ph: obs_all[mbinds],
+                    self.ac_ph: ac_all[mbinds],
+                    self.adv_ph: advs,
+                    self.ret_ph: ret_all[mbinds],
+                    self.neglogp_old_ph: neglogp_all[mbinds],
+                    self.val_ph: val_all[mbinds],
+                    self.lr_ph: lr,
+                    self.beta_ph: self.beta
+                }
+
+                infos, losses, _ = self.sess.run(
+                    [self.infos, self.losses, self.train_op], feed_dict=inputs)
+                # Unpack losses
+                kl_penalty, pi_loss, vf_loss, ent, kl = losses
+                pi_loss_buf.append(pi_loss)
+                vf_loss_buf.append(vf_loss)
+                ent_buf.append(ent)
+                kl_buf.append(kl)
+                kl_penalty_buf.append(kl_penalty)
+                # Unpack infos
+                ratio, ratioclipfrac, gradclipped = infos
+                ratio_buf.append(ratio)
+                ratioclipfrac_buf.append(ratioclipfrac)
+                gradclipped_buf.append(gradclipped)
+
+        kl_penalty_mean = np.mean(kl_penalty_buf)
+
+        if kl_penalty_mean > self.target_kl * 1.5:
+            self.beta *= 2
+        elif kl_penalty_mean < self.target_kl / 1.5:
+            self.beta /= 2
+        self.beta = np.clip(self.beta, 2**(-10), 64)
+
+        # Here you can add any information you want to log!
+        logger.logkv("loss/gradclipfrac", np.mean(gradclipped))
+        logger.logkv("loss/lr", lr)
+        logger.logkv("loss/ratio", np.mean(ratio_buf))
+        logger.logkv("loss/ratioclipfrac", np.mean(ratioclipfrac_buf))
+        logger.logkv("loss/kl_penalty", np.mean(kl_penalty_buf))
+        logger.logkv("loss/beta", self.beta)
+
+        return [np.mean(pi_loss_buf), np.mean(vf_loss_buf),
+                np.mean(ent_buf), np.mean(kl_buf)]
+
+    def select_action(self, obs, deterministic=False):
+        [mu, pi, v, neglogp] = self.sess.run(
+            self.get_action_ops, feed_dict={self.ob1_ph: obs.reshape(1, -1)})
+        ac = mu if deterministic else pi
+        return pi, v, neglogp
+
+    def compute_v(self, obs):
+        v = self.sess.run(
+            self.v1, feed_dict={self.ob1_ph: obs.reshape(1, -1)})
+        return v
