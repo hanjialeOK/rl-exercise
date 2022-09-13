f13bb2b635aa83537d19c8cd502388c7b4187b17
hanjiale <hanjiale@mail.ustc.edu.cn>
2022-09-13 13:45:04 +0000

    Add ACERBuffer and update GAEVBuffer.

--- pg/buffer/gaebuffer_atari.py
+++ pg/buffer/gaebuffer_atari.py
@@ -71,7 +71,6 @@ class GAEBuffer:
 
         # Reset ptr
         self.ptr = 0
-        self.path_start_idx = self.ptr
 
         return [sf01(self.obs_buf),
                 sf01(self.ac_buf),
@@ -95,7 +94,7 @@ class GAEVBuffer:
         ac_shape = env.action_space.shape
         obs_dtype = env.observation_space.dtype.name
         ac_dtype = env.action_space.dtype.name
-        max_size = horizon * nlatest // nenv
+        max_size = horizon * nlatest
         self.obs_buf = np.zeros((max_size, nenv) + obs_shape, dtype=obs_dtype)
         self.obs2_buf = np.zeros((max_size, nenv) + obs_shape, dtype=obs_dtype)
         self.ac_buf = np.zeros((max_size, nenv) + ac_shape, dtype=ac_dtype)
@@ -113,6 +112,7 @@ class GAEVBuffer:
         self.horizon = horizon
         self.nlatest = nlatest
         self.nenv = nenv
+        self.nbatch = horizon * nenv
         self.gamma = gamma
         self.lam = lam
         self.ptr = self.max_size - self.horizon
@@ -139,17 +139,29 @@ class GAEVBuffer:
         assert self.ptr == self.max_size
         assert self.count % self.horizon == 0 and self.count > 0
 
-        obs_all = self.obs_buf.reshape((self.max_size * self.nenv, ) + self.obs_shape)
-        val_all = self.compute_v_pik(obs_all)
-        self.val_buf[:] = val_all.reshape((self.max_size, self.nenv))
-        
-        obs2_all = self.obs2_buf.reshape((self.max_size * self.nenv, ) + self.obs_shape)
-        next_val_all = self.compute_v_pik(obs2_all)
-        self.next_val_buf[:] = next_val_all.reshape((self.max_size, self.nenv))
+        # obs_all = self.obs_buf.reshape((self.max_size * self.nenv, ) + self.obs_shape)
+        for i in range(self.nlatest):
+            # batch_inds = slice(i*self.nbatch, (i+1)*self.nbatch)
+            mbinds = slice(i*self.horizon, (i+1)*self.horizon)
+            obs_batch = self.obs_buf[mbinds].reshape((self.horizon * self.nenv, ) + self.obs_shape)
+            val_batch = self.compute_v_pik(obs_batch)
+            self.val_buf[mbinds] = val_batch.reshape((self.horizon, self.nenv))
 
-        ac_all = self.ac_buf.reshape((self.max_size * self.nenv))
-        neglogp_pik_all = self.compute_neglogp_pik(obs_all, ac_all)
-        self.neglogp_pik_buf[:] = neglogp_pik_all.reshape((self.max_size, self.nenv))
+        # obs2_all = self.obs2_buf.reshape((self.max_size * self.nenv, ) + self.obs_shape)
+        # for i in range(self.nlatest):
+            # batch_inds = slice(i*self.nbatch, (i+1)*self.nbatch)
+            # mbinds = slice(i*self.horizon, (i+1)*self.horizon)
+            obs2_batch = self.obs2_buf[mbinds].reshape((self.horizon * self.nenv, ) + self.obs_shape)
+            next_val_batch = self.compute_v_pik(obs2_batch)
+            self.next_val_buf[mbinds] = next_val_batch.reshape((self.horizon, self.nenv))
+
+        # ac_all = self.ac_buf.reshape((self.max_size * self.nenv))
+        # for i in range(self.nlatest):
+            # batch_inds = slice(i*self.nbatch, (i+1)*self.nbatch)
+            # mbinds = slice(i*self.horizon, (i+1)*self.horizon)
+            ac_batch = self.ac_buf[mbinds].reshape((self.horizon * self.nenv))
+            neglogp_pik_batch = self.compute_neglogp_pik(obs_batch, ac_batch)
+            self.neglogp_pik_buf[mbinds] = neglogp_pik_batch.reshape((self.horizon, self.nenv))
 
         rho = np.exp(self.neglogp_buf - self.neglogp_pik_buf)
         # Reduce bias here!
@@ -167,7 +179,6 @@ class GAEVBuffer:
 
         # Reset ptr
         self.ptr = self.max_size - self.horizon
-        self.path_start_idx = self.ptr
 
         return [sf01(self.obs_buf[-self.count:]),
                 sf01(self.ac_buf[-self.count:]),
@@ -195,3 +206,113 @@ class GAEVBuffer:
     def reset(self):
         self.ptr = self.max_size - self.horizon
         self.count = 0
+
+
+class ACERBuffer:
+    '''
+    Openai spinningup implementation
+    '''
+
+    def __init__(self, env, horizon, replay_size=5000, gamma=0.99,
+                 compute_v=None, compute_sdn_pik=None):
+        obs_shape = env.observation_space.shape
+        ac_shape = env.action_space.shape
+        obs_dtype = env.observation_space.dtype.name
+        ac_dtype = env.action_space.dtype.name
+        max_size = replay_size
+        self.nlatest = replay_size // horizon
+        self.obs_buf = np.zeros((max_size, ) + obs_shape, dtype=obs_dtype)
+        self.obs2_buf = np.zeros((max_size, ) + obs_shape, dtype=obs_dtype)
+        self.raw_obs_buf = np.zeros((max_size, ) + obs_shape, dtype=obs_dtype)
+        self.raw_obs2_buf = np.zeros((max_size, ) + obs_shape, dtype=obs_dtype)
+        self.ac_buf = np.zeros((max_size, ) + ac_shape, dtype=ac_dtype)
+        self.rew_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.raw_rew_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.done_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.ret_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.adv_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.val_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.qval_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.qret_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.qopc_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.next_val_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.neglogp_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.neglogp_pik_buf = np.zeros((max_size, ), dtype=np.float32)
+        self.mean_buf = np.zeros((max_size, ) + ac_shape, dtype=np.float32)
+        self.logstd_buf = np.zeros((max_size, ) + ac_shape, dtype=np.float32)
+        self.obs_shape = obs_shape
+        self.ac_shape = ac_shape
+        self.max_size = max_size
+        self.horizon = horizon
+        self.gamma = gamma
+        self.ptr = 0
+        self.count = 0
+        self.obfilt = env._obfilt
+        self.rewfilt = env._rewfilt
+        self.compute_v = compute_v
+        self.compute_sdn_pik = compute_sdn_pik
+        self.n_trajs = 0
+
+    def store(self, obs=None, ac=None, rew=None, done=None, obs2=None, val=None, neglogp=None,
+              raw_obs=None, raw_rew=None, raw_obs2=None, mean=None, logstd=None):
+        assert self.ptr < self.max_size
+        # assert obs.shape == self.obs_shape
+        # assert raw_obs.shape == self.obs_shape
+        # assert ac.shape == self.ac_shape
+        self.obs_buf[self.ptr] = obs
+        self.obs2_buf[self.ptr] = obs2
+        self.ac_buf[self.ptr] = ac
+        self.rew_buf[self.ptr] = rew
+        self.done_buf[self.ptr] = done
+        self.raw_obs_buf[self.ptr] = raw_obs
+        self.raw_obs2_buf[self.ptr] = raw_obs2
+        self.raw_rew_buf[self.ptr] = raw_rew
+        self.val_buf[self.ptr] = val
+        self.neglogp_buf[self.ptr] = neglogp
+        self.mean_buf[self.ptr] = mean
+        self.logstd_buf[self.ptr] = logstd
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.count = min(self.count+1, self.max_size)
+
+    def finish_path(self):
+        self.n_trajs = min(self.nlatest, self.n_trajs + 1)
+
+    def get(self, on_policy=False):
+        assert self.ptr % self.horizon == 0
+        assert self.count % self.horizon == 0 and self.count > 0
+
+        if self.obfilt:
+            self.obs_buf[:] = self.obfilt(self.raw_obs_buf)
+            self.obs2_buf[:] = self.obfilt(self.raw_obs2_buf)
+        if self.rewfilt:
+            self.rew_buf[:] = self.rewfilt(self.raw_rew_buf)
+
+        if on_policy:
+            pos = self.nlatest - 1 if self.ptr == 0 else self.ptr // self.horizon - 1
+        else:
+            pos = np.random.randint(0, self.count // self.horizon)
+        start = pos * self.horizon
+        end = (pos + 1) * self.horizon
+
+        last_ob = self.obs2_buf[end-1].reshape(1, -1)
+        obs = np.append(self.obs_buf[start:end], last_ob, axis=0)
+
+        return [obs,
+                self.ac_buf[start:end],
+                self.rew_buf[start:end],
+                self.done_buf[start:end],
+                self.neglogp_buf[start:end],
+                self.mean_buf[start:end],
+                self.logstd_buf[start:end]]
+
+    def get_rms_data(self):
+        raise NotImplementedError
+        assert self.ptr == self.max_size
+        # Return the latest RMS data
+        return [self.obs_buf[-self.horizon:],
+                self.ret_buf[-self.horizon:]]
+
+    def reset(self):
+        self.ptr = 0
+        self.count = 0
+        self.n_trajs = 0
